c6a497df75595ad1de7468b471ebc9ff47ab622d
Alexander Kelso <alexander.kelso@outlook.com>
2020-11-20 11:35:09 +1100

    updated root directory to cocktails#index

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'json'
+require 'open-uri'
+
+
+puts "seeding ingredients list"
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+file = open(url).read
+drinks_hash = JSON.parse(file)
+ingredients_list = drinks_hash["drinks"]
+ingredients_list.each do |ingredient|
+  Ingredient.create!(name: ingredient["strIngredient1"])
+end
+
+puts "Ingredient's list done mofo!"
+
+
