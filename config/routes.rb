230018ee66062c8c74b40899bbfb8dab5892c71b
Alexander Kelso <alexander.kelso@outlook.com>
2020-11-20 13:46:31 +1100

    updated root index spelling error

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'coctails#index'
+  root to: 'cocktails#index'
   resources :cocktails, only: [ :index, :show, :create, :new ] do
     resources :doses, only: [ :new, :create]
   end
